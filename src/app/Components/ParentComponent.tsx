bd3f7704c0a9e1e6cf1f4def63658c7d4403ea85
Umm e Aiman <ummeaiman207@gmail.com>
2024-11-27 13:19:21 -0800

    first commit

---
+++ src/app/Components/ParentComponent.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ChildComponent from "./ChildComponent";
+
+function ParentComponent(){
+   const cars = [
+    {
+      carImage: '/car1.png',
+      carName: 'Alto',
+      carPrice: 90000,
+      carReviews: 4.8,
+    },
+    {
+      carImage: '/car2.jpg',
+      carName: 'Toyota Corolla',
+      carPrice: 70000,
+      carReviews: 4.5,
+    },
+    {
+      carImage: '/car3.jpg',
+      carName: 'Honda City',
+      carPrice: 650000,
+      carReviews: 4.7,
+    },
+    {
+      carImage: '/car4.jpg',
+      carName: 'Honda Civic',
+      carPrice: 55000,
+      carReviews: 4.6,
+    },
+   ];
+  return (
+    <div>
+      {cars.map((car => {
+        return(
+          <ChildComponent
+          carImage={car.carImage}
+          carName={car.carName}
+          carPrice={car.carPrice}
+          carReviews={car.carReviews}
+          />
+        )
+      }))}
+    </div>
+   )
+}
+
+export default ParentComponent
