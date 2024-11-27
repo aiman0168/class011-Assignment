d421307894a0d32b56c6beaf4ed10145d83dcce8
Umm e Aiman <160779331+aiman0168@users.noreply.github.com>
2024-11-27 13:43:04 -0800

    Update ChildComponent.tsx

--- src/app/Components/ChildComponent.tsx
+++ src/app/Components/ChildComponent.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Image from "next/image";
 
-function ChildComponent(props: any) {
+interface MyObject {
+  carImage: string;
+  carName: string;
+  carPrice: number;
+  carReviews: number;
+}
+
+function ChildComponent(props: MyObject) {
   return (
     <div className="flex flex-col items-center border border-gray-300 p-5 m-4 w-64">
-      <Image src={props.carImage} alt={props.carName} className="w-full h-auto mb-4" />
+      <Image src={props.carImage} alt={props.carName} width={240} height={240} className="w-full h-auto mb-4" />
       <h2 className="text-xl font-semibold text-gray-800 mb-2">{props.carName}</h2>
       <p className="text-gray-600 mb-2">Price: {props.carPrice}</p>
       <p className="text-yellow-500">{props.carReviews} ⭐</p>
